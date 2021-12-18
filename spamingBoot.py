59d49ba073d76949465d24d956e22313d2fad9bc
LavaiceDragon <weidner.florian99@protonmail.com>
2021-12-18 20:28:42 +0100

    Add Class SpammingBoot

--- spamingBoot.py
+++ spamingBoot.py
@@ -1,3 +1,16 @@
 import discord
 import conf
 
+
+
+
+class SpamingBoot(discord.Client):
+    async def on_ready(self):
+        print("ich habe mich eingeloggt.")
+
+    async def on_message(self, message):
+          pass
+
+if __name__ == '__main__':
+    client = SpamingBoot()
+    client.run(conf.sAPIkey)
